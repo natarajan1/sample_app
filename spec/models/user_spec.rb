6d0172599840d174d889430abebdbea9e1166422
natarajan <natarajan@arrivusystem.com>
2013-03-05 13:58:51 +0530

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,7 +11,7 @@
 
 require 'spec_helper'
 
-describe User do
+describe User do 
 	#before { @user = User.new(name: "Example User", email: "user@example.com")}
 
 	before do
@@ -107,7 +107,20 @@ describe User do
 			it { should_not == user_for_invalid_password }
 			specify { user_for_invalid_password.should be_false }
 		end
+
+		describe "remember token" do
+			before { @user.save }
+			its (:remember_token) { should_not be_blank }
+			
+		end
+	
+			it { should respond_to(:password_confirmation) }
+
+			it { should respond_to(:remember_token) }
+			it { should respond_to(:authenticate) }
+
 	end
+
 
 end
 
